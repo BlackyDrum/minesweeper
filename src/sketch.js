878f35b8a3c56d0128afbd90ae4474c60c59bbe5
BlackyDrum <ganiaytan1010@web.de>
2023-12-26 21:42:25 +0100

    Add bomb and number calculations

--- src/sketch.js
+++ src/sketch.js
@@ -1,7 +1,30 @@
+let grid = [];
+
+let bomb;
+let flag;
+
+const bombCount = 40;
+
+function preload() {
+  bomb = loadImage("assets/Bomb.png");
+  flag = loadImage("assets/Flag.png");
+}
+
 function setup() {
   let canvas = createCanvas(600, 600);
   canvas.style("border-width: 10px");
   canvas.style("border-color: gray");
+
+  for (let i = 0; i < 20; i++) {
+    grid.push([]);
+    for (let j = 0; j < 20; j++) {
+      grid[i].push(0);
+    }
+  }
+
+  calculateBombPosition();
+  calculateNumbers();
+  console.table(grid);
 }
 
 function draw() {
@@ -25,6 +48,60 @@ function draw() {
       fill(120);
       rect(i * cellWidth, (j + 1) * cellHeight - borderSize, cellWidth, borderSize);
       rect((i + 1) * cellWidth - borderSize, j * cellHeight, borderSize, cellHeight);
+    }
+  }
+}
+
+function calculateBombPosition() {
+  for (let i = 0; i < bombCount; i++) {
+    let options = [];
+
+    for (let i = 0; i < 20; i++) {
+      for (let j = 0; j < 20; j++) {
+        if (grid[i][j] === 0) {
+          options.push({ x: i, y: j });
+        }
+      }
+    }
+
+    let index = int(random(0, options.length - 1));
+    let pos = options[index];
+    grid[pos.x][pos.y] = -1; // -1 represents a bomb
+  }
+}
+
+function calculateNumbers() {
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 20; j++) {
+      if (grid[i][j] === -1) continue;
+
+      let currentNum = 0;
+      if (i - 1 >= 0 && grid[i - 1][j] === -1) {
+        currentNum++;
+      }
+      if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] === -1) {
+        currentNum++;
+      }
+      if (i - 1 >= 0 && j + 1 < grid.length && grid[i - 1][j + 1] === -1) {
+        currentNum++;
+      }
+      if (i + 1 < grid.length && grid[i + 1][j] === -1) {
+        currentNum++;
+      }
+      if (i + 1 < grid.length && j - 1 >= 0 && grid[i + 1][j - 1] === -1) {
+        currentNum++;
+      }
+      if (i + 1 < grid.length && j + 1 < grid.length && grid[i + 1][j + 1] === -1) {
+        currentNum++;
+      }
+      if (j - 1 >= 0 && grid[i][j - 1] === -1) {
+        currentNum++;
+      }
+      if (j + 1 < grid.length && grid[i][j + 1] === -1) {
+        currentNum++;
+      }
+
+      grid[i][j] = currentNum;
     }
   }
 }
